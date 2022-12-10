cb769687efad9fdf43782297a9784ead9720d95d
Quintangeli <108161984+Quintangeli@users.noreply.github.com>
2022-12-09 19:51:55 -0600

    Added comments

--- HowToOLED.ino
+++ HowToOLED.ino
@@ -7,11 +7,11 @@
 Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.display();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);    //The typical Adress is 0x3C or 0x3D
+  display.clearDisplay();   //removes all data from buffer
+  display.display();    //loads your buffer to the screen
   display.setTextSize(0);
-  display.setCursor(65, 14);
+  display.setCursor(65, 14);    //Sets the starting point on an XY coordinate
   display.print("TEST");
   display.display();
 
